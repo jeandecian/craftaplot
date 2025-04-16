ba3f9e3cbca8c59a89c9f6aa0c9afd21b10c47b1
Jean Decian <jeandecian@hotmail.fr>
2025-04-15 23:35:00 -0400

    add settings and plot panels

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QLabel, QMainWindow, QWidget
 import sys
 
 
@@ -8,6 +8,20 @@ class CraftAPlotApp(QMainWindow):
 
         self.setWindowTitle("CraftAPlot")
         self.setGeometry(100, 100, 800, 600)
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        self.layout = QHBoxLayout(self.central_widget)
+
+        self.init_settings_panel()
+        self.init_plot_panel()
+
+    def init_settings_panel(self):
+        self.layout.addWidget(QLabel("Settings Panel"))
+
+    def init_plot_panel(self):
+        self.layout.addWidget(QLabel("Plot Panel"))
 
 
 def main():
